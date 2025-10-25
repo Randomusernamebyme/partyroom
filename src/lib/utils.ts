d9ae4fb4d9eed2fc86b9a04a6a0b48f26d6b3d63
Randomusernamebyme <jiayiannie@gmail.com>
2025-10-26 03:40:37 +0800

    Fix utils.ts: restore cn function and add game utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 export const getItemTypeIcon = (type: string) => {
   const typeMap: { [key: string]: string } = {
     game: '🎮',
